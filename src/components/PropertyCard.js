291686f2363e124c9339d99309c8c08e70e6b985
AnnaE <annaefitzgerald@gmail.com>
2021-11-26 22:04:20 -0600

    submit user object property to moralis db

--- src/components/PropertyCard.js
+++ src/components/PropertyCard.js
@@ -1,22 +1,44 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { useMoralis } from 'react-moralis';
-
+import { Moralis } from 'moralis';
 
 
 
 const PropertyCard = () => {
 
-    const { setUserData, userError, isUserUpdating, user, property } = useMoralis();
-    // const AquilaUser = Moralis.Object.extend("User");
-    
+    let navigate = useNavigate();
+    const AquilaUser = Moralis.Object.extend("User");
+    const user = new AquilaUser();
+    const { setUserData, userError } = useMoralis();
+
+    const changeHandler = e => {
+        setUserData({ 
+            [e.target.name]:e.target.value
+        })
+    }
+    async function submitHandler(e) {
+        e.preventDefault();
+        let result = await user.save(e.target)
+        if (result.userError) {
+            console.log(userError)
+        } else {
+            navigate('/premium');
+        }
+    }
+        
 
     return (
         <div className="card">
-            <form>
+            <form onSubmit = { submitHandler } autocomplete="off">
                 { userError && <p> {userError.message }</p>}
+                
                 <label>Longitude and Latitude</label>
-                <textarea name="property" id="property"/>
-                <input type="submit" className="btn" onClick={() => setUserData({ user: user, property: property })} disabled={ isUserUpdating } />
+
+                <textarea name="property" id="property" onChange={ changeHandler} />
+
+                <input type="submit" className="btn" />
+
             </form> 
         </div>
     );
